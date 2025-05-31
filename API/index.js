ef541c8ce37847d06fd7d6157793f47610dd6c27
DANILO BENEDETTI RIBEIRO <1091392313033@fatec-srv.local>
2025-05-31 11:57:02 -0300

    teste7

--- API/index.js
+++ API/index.js
@@ -1,10 +1,45 @@
 const Express = require('express');
 const app = Express();
+const cors = require('cors');
 const connection = require("./database/connection");
 const homeController = require("./home/homeController");
 const bodyParser = require('body-parser');
 const porta = 4000;
 
+
+// Configuração de CORS
+const corsOptions = {
+  origin: function (origin, callback) {
+      const allowedOrigins = [
+          'http://localhost:3000',
+          'http://localhost:8081',
+          'http://13.68.97.186:3000',
+          'http://13.68.97.186',
+          'http://localhost:8080',
+          /^http:\/\/localhost:\d+$/,
+          '*'
+      ];
+
+      if (!origin || allowedOrigins.some(allowed => 
+          (typeof allowed === 'string' && allowed === origin) || 
+          (allowed instanceof RegExp && allowed.test(origin))
+      )) {
+          callback(null, true);
+      } else {
+          callback(new Error('Origem não permitida pelo CORS'));
+      }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Origin',
+      'X-Requested-With',
+      'Accept'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
 
 // engine vai ser ejs
 app.set("view engine", "ejs");
