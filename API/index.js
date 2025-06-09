2959fb7b6e83d5183f10ac047a4eff83ec51246c
DanBenedetti <danilobenedetti1@gmail.com>
2025-06-08 21:12:24 -0300

    alteração no index.js

--- API/index.js
+++ API/index.js
@@ -70,7 +70,7 @@ app.use(bodyParser.json());
 
 app.use("/", homeController);
 
-app.listen(porta, '0.0.0.0', () => {
+app.listen(porta, () => {
 
     console.log("Servidor rodando na porta: " + porta);
 
